dbb55e9424be7b39c51a40b8a47b568676bca5e0
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-01-05 16:56:07 +0000

    Ajouter les apiPlatform

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -6,8 +6,10 @@ use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
+#[ApiResource]
 class Customer
 {
     #[ORM\Id]
